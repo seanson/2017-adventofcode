92a5a8a8667f75886aa6c8b7d2b0df998e5b9d51
Johnson <Sean Johnson seanson@users.noreply.github.com>
2017-12-06 22:45:18 +1100

    Rustfmt updating

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -9,28 +9,30 @@ fn str_to_vec(input: &String) -> Vec<u32> {
 fn zip_offset_vec(items: &Vec<u32>, offset: usize) -> Vec<u32> {
     // Creates an offset vector, zips the items and returns a reduced vector of matched pairs
     let mut a_sub: Vec<u32> = Vec::new();
-    a_sub.extend(&items[items.len()-offset..]);
-    a_sub.extend(&items[..items.len()-offset]);
-    items.iter()
-         .zip(a_sub.iter())
-         .map(|(&x, &y)| match x == y {
-                                        true => x,
-                                        false => 0,
-                                      })
-         .collect()
+    a_sub.extend(&items[items.len() - offset..]);
+    a_sub.extend(&items[..items.len() - offset]);
+    items
+        .iter()
+        .zip(a_sub.iter())
+        .map(|(&x, &y)| match x == y {
+            true => x,
+            false => 0,
+        })
+        .collect()
 }
 
 
 fn main() {
     let mut f = File::open("src/input.txt").expect("file not found");
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
     let items: Vec<u32> = str_to_vec(&contents);
     let results = zip_offset_vec(&items, 1);
     let sum: u32 = results.iter().sum();
     println!("Offset sum 1: {}", sum);
-    let results = zip_offset_vec(&items, items.len()/2);
+    let results = zip_offset_vec(&items, items.len() / 2);
     let sum: u32 = results.iter().sum();
     println!("Offset sum 1/2 list: {}", sum);
 }
--- 2/src/main.rs
+++ 2/src/main.rs
@@ -2,7 +2,9 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn parse_line(line: &str) -> Vec<u32> {
-    line.split_whitespace().map(|s| s.parse().unwrap()).collect()
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
 }
 
 fn parse_file(file: &str) -> Vec<Vec<u32>> {
@@ -12,35 +14,33 @@ fn parse_file(file: &str) -> Vec<Vec<u32>> {
 fn vec_to_checksum(list: &Vec<u32>) -> u32 {
     let mut new_list: Vec<u32> = list[..].to_vec();
     new_list.sort();
-    new_list[new_list.len()-1] - new_list[0]
+    new_list[new_list.len() - 1] - new_list[0]
 }
 
 
 fn vec_to_div(list: &Vec<u32>) -> u32 {
     list.iter()
-        .map(|&x| list.iter()
-                      .map(|&y| match x % y == 0 && x != y {
-                                    true => x / y,
-                                    false => 0
-                                    }
-                          )
-                      .sum::<u32>()
-        ).sum()
+        .map(|&x| {
+            list.iter()
+                .map(|&y| match x % y == 0 && x != y {
+                    true => x / y,
+                    false => 0,
+                })
+                .sum::<u32>()
+        })
+        .sum()
 }
 
 
 fn main() {
     let mut f = File::open("src/input.txt").expect("file not found");
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
     let items: Vec<Vec<u32>> = parse_file(&contents);
-    let result_1: u32 = items.iter()
-                              .map(|row| vec_to_checksum(&row))
-                              .sum();
+    let result_1: u32 = items.iter().map(|row| vec_to_checksum(&row)).sum();
     println!("Checksum 1: {}", result_1);
-    let result_2: u32 = items.iter()
-                             .map(|row| vec_to_div(&row))
-                             .sum();
+    let result_2: u32 = items.iter().map(|row| vec_to_div(&row)).sum();
     println!("Checksum 2: {}", result_2);
 }
